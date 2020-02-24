c3e4c1dfe5f4313be0ac6aefd43f851d7938401b
Michael Lingelbach <m.j.lbach@gmail.com>
2020-02-24 08:52:42 -0800

    Should run in nixpkgs

--- run_bisect.sh
+++ run_bisect.sh
@@ -1,4 +1,3 @@
-export NIXPKGS=$HOME/Repositories/nixpkgs
-git --git-dir=$NIXPKGS/.git  bisect good 4fdea7312e8cfe13306cd8d70ee6b060dae956e3
-git --git-dir=$NIXPKGS/.git bisect bad master
-git --git-dir=$NIXPKGS/.git bisect run ./test.sh
+git bisect good 4fdea7312e8cfe13306cd8d70ee6b060dae956e3
+git bisect bad master
+git bisect run ./test.sh
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cp default.nix.bak $NIXPKGS/pkgs/development/tools/build-managers/bazel/default.nix
 cp ./src-deps.json $NIXPKGS/pkgs/development/tools/build-managers/bazel/src-deps.json
-nix-build $NIXPKGS -A bazel
-git --git-dir=$NIXPKGS/.git reset --hard 
+nix-build -A bazel
+git reset --hard 
 status=$?
 exit $status
