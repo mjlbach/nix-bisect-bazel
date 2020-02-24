b76311b996e1ea7e63f9a8c47c125c38c4fc5502
Michael Lingelbach <m.j.lbach@gmail.com>
2020-02-24 00:13:40 -0800

    Update

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 cp default.nix.bak $NIXPKGS/pkgs/development/tools/build-managers/bazel/default.nix
 cp ./src-deps.json $NIXPKGS/pkgs/development/tools/build-managers/bazel/src-deps.json
-mkdir tmp && cd tmp
 nix-build $NIXPKGS -A pkgs.bazel
-cd .. && rm -rf tmp
 git --git-dir=$NIXPKGS/.git reset --hard 
 status=$?
 exit $status
